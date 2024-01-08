475aa05285ea77c3f196ab3f83d22415fd8f556c
Sumanth kumar <112607313+Sumanthkumar2002@users.noreply.github.com>
2024-01-08 18:53:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,79 +1,74 @@
-import cv2
 import numpy as np
+import cv2
+import streamlit as st
+from tensorflow import keras
 from keras.models import model_from_json
-
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 emotion_dict = {0: "You are not confident", 1: "You are not confident", 2: "You are not confident", 3: "You are Confident", 4: "Need to work on confidence", 5: "You are not confident", 6: "You are not confident"}
 
 # load json and create model
-json_file = open('./emotion_model.json', 'r')
+json_file = open('emotion_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
-emotion_model = model_from_json(loaded_model_json)
+classifier = model_from_json(loaded_model_json)
 
 # load weights into new model
-emotion_model.load_weights("./emotion_model.h5")
-print("Loaded model from disk")
+classifier.load_weights("emotion_model.h5")
 
-# start the webcam feed
-cap = cv2.VideoCapture(0)
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
 
-# pass here your video path
-# you may download one from here : https://www.pexels.com/video/three-girls-laughing-5273028/
-#cap = cv2.VideoCapture("path")
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return img
+
+def main():
+    # Face Analysis Application #
+    st.title("Emotion Crawler")
+    activiteis = ["Home", "Confidence Analyzer"]
+    choice = st.sidebar.selectbox("Select Activity", activiteis)
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Person Confidence Detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+    elif choice == "Confidence Analyzer":
+        st.header("Confidence Analyzing")
+        st.write("Click on start to use webcam and detect your Confidence")
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    else:
+        pass
 
 
-#new code for define new function of calculating most
-def most_frequent(emotion_dict_store):
-    counter = 0
-    num = emotion_dict_store[0]
-    for i in emotion_dict_store:
-        curr_frequency = emotion_dict_store.count(i)
-        if(curr_frequency > counter):
-            counter = curr_frequency
-            num = i
-    return num
-
-while True:
-    # Find haar cascade to draw bounding box around face
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (1280, 720))
-    if not ret:
-        break
-    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # detect faces available on camera
-    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
-
-    # take each face available on the camera and Preprocess it
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-
-        # predict the emotions
-        emotion_prediction = emotion_model.predict(cropped_img)
-        maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-
-
-        emotion_dict_store = []
-        emotion_dict_store.append(emotion_dict[maxindex]) 
-    
-    cv2.imshow('Emotion/Confidence Detection', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-print('dominant emotion detected:-')
-print(most_frequent(emotion_dict_store))
-confidence=most_frequent(emotion_dict_store)
-if confidence == "Happy":
-    print('You are confident enough')
-elif confidence == "Neutral":
-    print('you need to work on your confidence')
-else:
-    print('you are not confident enough')
-        
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
